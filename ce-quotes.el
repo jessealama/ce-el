823ab887446b0633d33d32a897f7ae6ac030f7a0
Jesse Alama <jesse.alama@gmail.com>
2011-10-29 02:22:44 +0200

    Three options: yes, no, or quit.

--- ce-quotes.el
+++ ce-quotes.el
@@ -2,7 +2,20 @@
   "A regular expression matching the quotes we need to treat.")
 
 (defun ce-quote-num-candidates ()
-  (how-many *ce-quote-regexp*))
+  (save-excursion
+    (goto-char (point-min))
+    (how-many *ce-quote-regexp*)))
+
+(defun y-n-or-q (message)
+  (let (input)
+    (while  (not (or (string= input "y")
+		     (string= input "Y")
+		     (string= input "n")
+		     (string= input "N")
+		     (string= input "q")
+		     (string= input "Q")))
+      (setf input (read-from-minibuffer message)))
+    (setf input (downcase input))))
 
 (defun ce-quote-fix-quotes ()
   (interactive)
@@ -10,10 +23,28 @@
     (if (zerop num-candidates-remaining)
 	(message "No candidate quotes to inspect.")
       (save-excursion
+	(if (> num-candidates-remaining 1)
+	    (message "Inspecting %d posessive quote candidates..."
+		     num-candidates-remaining)
+	  (message "Inspecting 1 possessive quote candidate..."))
 	(goto-char (point-min))
 	(while (re-search-forward *ce-quote-regexp* nil t)
-	  (when (or (looking-at " ")
-		    (looking-at "$"))
-	    (when (y-or-n-p "Replace this occurrence of 's by &apos;s? ")
-	      (delete-char 2)
-	      (insert "&apos;s"))))))))
+	  (decf num-candidates-remaining)
+	  (goto-char (match-beginning 0))
+	  (let ((message (concat "Replace 's by &apos;s? "
+				 (if (= num-candidates-remaining 1)
+				     "[1 candidate remaining] "
+				   (format "[%d candidates remaining] "
+					   num-candidates-remaining))
+				 "([y]es, [n]o, [q]uit) ")))
+	    (let ((response (y-n-or-q message)))
+	      (cond ((string= response "y")
+		     (delete-char 2)
+		     (insert "&apos;s"))
+		    ((string= response "n")
+		     (forward-char 2))
+		    ((string= response "q")
+		     (goto-char (point-max)))
+		    (t
+		     (message "Unable to process the reponse '%s'; quitting" response)
+		     (goto-char (point-max)))))))))))

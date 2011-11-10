776e5397fc640c42b065f4ec5d6e036f208db455
Jesse Alama <jesse.alama@gmail.com>
2011-11-10 20:30:21 +0100

    Start fixing right quote entities.
    Still doesn't work.

--- ce-quotes.el
+++ ce-quotes.el
@@ -1,12 +1,20 @@
 ;;; ce-quotes.el --- The variety of quotes
 
-(defvar *ce-quote-regexp* "[']\\(.\\)"
+(defvar *ce-quote-sharp-quote-regexp* "[']\\(.\\)"
   "A regular expression matching the quotes we need to treat.")
 
-(defun ce-quote-num-candidates ()
+(defvar *ce-quote-right-quote-entity-regexp* "\\(&rsquo;\\)|\\(&rdquo;\\)\\(.\\)"
+  "A regular expression matching the entities of right quotes.")
+
+(defun ce-quote-num-sharp-quote-candidates ()
   (save-excursion
     (goto-char (point-min))
-    (how-many *ce-quote-regexp*)))
+    (how-many *ce-quote-sharp-quote-regexp*)))
+
+(defun ce-quote-num-quote-entity-candidates ()
+  (save-excursion
+    (goto-char (point-min))
+    (how-many *ce-quote-right-quote-entity-regexp*)))
 
 (defvar *ce-quote-keymap*
   (let ((map (make-sparse-keymap)))
@@ -41,7 +49,8 @@
   t)
 
 (defun ce-quote-fix-sharp-quotes ()
-  (let ((num-candidates-remaining (ce-quote-num-candidates)))
+  (let ((num-candidates-remaining (ce-quote-num-sharp-quote-candidates))
+	(num-fixed 0))
     (if (zerop num-candidates-remaining)
 	(message "No candidate quotes to inspect.")
       (save-excursion
@@ -50,7 +59,7 @@
 		     num-candidates-remaining)
 	  (message "Inspecting 1 sharp quote candidate..."))
 	(goto-char (point-min))
-	(while (re-search-forward *ce-quote-regexp* nil t)
+	(while (re-search-forward *ce-quote-sharp-quote-regexp* nil t)
 	  (decf num-candidates-remaining)
 	  (goto-char (match-beginning 0))
 	  (let* ((after-quote-char (match-string-no-properties 1))
@@ -69,7 +78,74 @@
 		(exit
 		 (goto-char (point-max)))
 		(act
+		 (incf num-fixed)
 		 (delete-char 2)
+		 (insert "&apos;")
+		 (insert after-quote-char))))))))
+    num-fixed))
+
+(defun ce-quote-count-quote (quote)
+  (save-excursion
+    (goto-char (point-min))
+    (how-many quote)))
+
+(defun ce-quote-count-lsquo ()
+  (ce-quote-count-quote "&lsquo;"))
+
+(defun ce-quote-count-rsquo ()
+  (ce-quote-count-quote "&rsquo;"))
+
+(defun ce-quote-count-ldquo ()
+  (ce-quote-count-quote "&ldquo;"))
+
+(defun ce-quote-count-rdquo ()
+  (ce-quote-count-quote "&rdquo;"))
+
+(defun ce-quote-check-balanced ()
+  (let ((lsquo (ce-quote-count-lsquo))
+	(rsquo (ce-quote-count-rsquo))
+	(ldquo (ce-quote-count-ldquo))
+	(rdquo (ce-quote-count-rdquo)))
+    (message "In this buffer, there are:
+* %d lsquo entities,
+* %d rsquo entities,
+* %d ldquo entities, and
+* %d rdquo entities" lsquo rsquo ldquo rdquo)))
+
+(defun ce-quote-fix-right-quote-entities ()
+  (let ((num-candidates-remaining (ce-quote-num-quote-entity-candidates)))
+    (if (zerop num-candidates-remaining)
+	(message "No candidate quotes to inspect.")
+      (save-excursion
+	(if (> num-candidates-remaining 1)
+	    (message "Inspecting %d right quote entity candidates..."
+		     num-candidates-remaining)
+	  (message "Inspecting 1 right quote entity candidate..."))
+	(goto-char (point-min))
+	(while (re-search-forward *ce-quote-right-quote-entity-regexp* nil t)
+	  (decf num-candidates-remaining)
+	  (goto-char (match-beginning 0))
+	  ;; ugh -- magic constant.  Look at the definition of the
+	  ;; regular expression to see why we choose 3 here
+	  (let* ((after-quote-char (match-string-no-properties 3))
+		 (message (concat (format "Replace '%s by &apos;%s? "
+					  after-quote-char
+					  after-quote-char)
+				  (if (= num-candidates-remaining 1)
+				      "[1 candidate remaining] "
+				    (format "[%d candidates remaining] "
+					    num-candidates-remaining))
+				  "([y]es, [n]o, [q]uit) ")))
+	    (let ((response (y-n-or-q message)))
+	      (ecase response
+		(skip
+		 (forward-char 2))
+		(exit
+		 (goto-char (point-max)))
+		(act
+		 ;; ugh.  Thankfully '&lsquo;' and '&rsquo;' have the
+		 ;; same length.
+		 (delete-char (length "&lsquo;"))
 		 (insert "&apos;")
 		 (insert after-quote-char))))))))))
 
@@ -77,6 +153,8 @@
   (interactive)
   ;; deal with non-ascii quotes
   (ce-quote-fix-non-ascii-quotes)
-  (ce-quote-fix-sharp-quotes))
+  (ce-quote-fix-sharp-quotes)
+  (ce-quote-fix-right-quote-entities)
+  (ce-quote-check-balanced))
 
 ;;; ce-quotes.el ends here

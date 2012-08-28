e79f1dac2805ec03feb2ea7e45922c77639da591
Jesse Alama <alama@Jesses-MacBook-Pro.local>
2012-08-28 10:55:54 +0200

    Move tidy utility to its own module.
    Eventually, we may not even use tidy.

--- ce.el
+++ ce.el
@@ -66,19 +66,6 @@ Encyclopedia of Philosophy."
   :tag "Copyediting"
   :group 'sep)
 
-;Function to run Tidy HTML parser on buffer
-; NOTE: this requires external Tidy program.
-(defun ce-tidy ()
-  "Run Tidy HTML parser on current buffer."
-  (interactive)
-  (if (get-buffer "tidy-errs") (kill-buffer "tidy-errs"))
-  (shell-command-on-region (point-min) (point-max)
-    "tidy -f /tmp/tidy-errs -q -i -wrap 72 -c" t)
-  (find-file-other-window "/tmp/tidy-errs")
-  (other-window 1)
-  (delete-file "/tmp/tidy-errs")
-  (message "buffer tidy'd"))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; The whole shebang
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

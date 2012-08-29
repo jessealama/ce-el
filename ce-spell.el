666d8cff7744fc9b8965875abdd6a72c98a20c2f
Jesse Alama <jesse.alama@gmail.com>
2012-08-29 11:34:40 +0200

    Make commands.

--- ce-spell.el
+++ ce-spell.el
@@ -3,9 +3,11 @@
 (require 'ispell)
 
 (defun ce-spell-check ()
+  (interactive)
   (ispell))
 
 (defun ce-spell-change-dictionary ()
+  (interactive)
   (ispell-change-dictionary))
 
 ;;; ce-spell.el ends here

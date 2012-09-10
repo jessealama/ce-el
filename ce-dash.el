6afa8a377fdfd8506171dbb56e54909449c49266
Jesse Alama <jesse.alama@gmail.com>
2012-09-10 11:08:32 +0200

    Rewrite the dash detector.

--- ce-dash.el
+++ ce-dash.el
@@ -2,65 +2,44 @@
 (require 'cl)
 (require 'nxml-mode) ; for navigating among XML tokens
 
-(defun ce-dash-first-dash-in-string (str)
-  (let ((dashes-pos (string-match "[-]+[^-]" str))
-	(ndash-pos (string-match "&ndash;" str))
-	(mdash-pos (string-match "&mdash;" str)))
-    (cond
-     (dashes-pos
-      (let ((dashes-end (string-match "[^-]" str dashes-pos)))
-	(cond ((and ndash-pos mdash-pos)
-	       (let ((min (min dashes-pos mdash-pos ndash-pos)))
-		 (cond ((= min dashes-pos)
-			(values dashes-pos dashes-end))
-		       ((= min mdash-pos)
-			(values mdash-pos (+ mdash-pos 7)))
-		       (t
-			(values ndash-pos (+ ndash-pos 7))))))
-	      (mdash-pos
-	       (if (< dashes-pos mdash-pos)
-		   (values dashes-pos dashes-end)
-		 (values mdash-pos (+ mdash-pos 7))))
-	      (ndash-pos
-	       (if (< dashes-pos ndash-pos)
-		   (values dashes-pos dashes-end)
-		 (values ndash-pos (+ ndash-pos 7))))
-	      (t
-	       (values dashes-pos dashes-end)))))
-     (ndash-pos
-      (if mdash-pos
-	  (let ((first (min mdash-pos ndash-pos)))
-	    (values first (+ first 7)))
-	(values mdash-pos (+ mdash-pos 7))))
-     (mdash-pos
-      (values mdash-pos (+ mdash-pos 7)))
-     (t (values nil nil)))))
+(defun ce-dash-is-dash-entity (entity-str)
+  (or (string= entity-str "&ndash;")
+      (string= entity-str "&mdash;")))
 
 (defun ce-dash-next-dash ()
   (let ((begin nil)
-	(end nil))
-    (save-excursion
-      (goto-char (point-min))
-      (let* ((begin (point-min))
-	     (previous-xml-token nil)
-	     (current-xml-token (xmltok-forward))
-	     (end (point)))
-	(while current-xml-token
-	  (when (and (eq previous-xml-token 'start-tag)
-		     (eq current-xml-token 'data))
-	    (let ((data (buffer-substring-no-properties begin end)))
-	      (multiple-value-bind (dash-begin dash-end)
-		  (ce-dash-first-dash-in-string data)
-		(when (and dash-begin dash-end)
-		  (setf begin dash-begin
-			end dash-end
-			current-xml-token nil)))))
-	  (setf begin end
-		end (point)
-		previous-xml-token current-xml-token
-		current-xml-token (xmltok-forward)))))
-    (when (and begin end)
-      (list begin end))))
+	(end nil)
+	(dash-begin nil)
+	(dash-end nil)
+	(dash-found nil))
+    (let* ((begin (point-min))
+	   (previous-xml-token nil)
+	   (current-xml-token (xmltok-forward))
+	   (end (point)))
+      (while (and current-xml-token
+		  (not dash-found))
+	(let ((data (buffer-substring-no-properties begin end)))
+	  (cond ((eq current-xml-token 'entity-ref)
+		 (when (ce-dash-is-dash-entity data)
+		   (setf dash-begin begin
+			 dash-end end
+			 dash-found t)))
+		((and (eq previous-xml-token 'start-tag)
+		      (eq current-xml-token 'data))
+		 (let ((dash-position (position ?\- data)))
+		   (when dash-position
+		     (let ((local-end (string-match "[^-]" data dash-position)))
+		       ;; (error "Data is '%s'%cdash-position is %d%cand the the local end is '%s'" data ?\n dash-position ?\n local-end)
+		       (setf dash-begin (+ begin dash-position))
+		       (setf dash-end (1- (+ begin local-end)))
+		       (setf dash-found t)))))))
+	(setf begin end
+	      previous-xml-token current-xml-token
+	      current-xml-token (xmltok-forward)
+	      end (point))))
+    (when dash-found
+      (goto-char dash-begin)
+      (list dash-begin dash-end))))
 
 (provide 'ce-dash)
 

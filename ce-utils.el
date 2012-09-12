1433cb03ccf79c3bcb0a951838ced8b4e1c6c955
Jesse Alama <jesse.alama@gmail.com>
2012-09-12 11:14:39 +0200

    New utility module.

---
+++ ce-utils.el
@@ -0,0 +1,18 @@
+
+(require 'cl)
+
+(defun member-of-some-array (thing list-of-arrays)
+  "Does THING belong to any of the arrays in LIST-OF-ARRAYS?"
+  (some #'(lambda (entity-array)
+	    (or (string= thing (aref entity-array 0))
+		(string= thing (aref entity-array 1))
+		(string= thing (aref entity-array 2))))
+	list-of-arrays))
+
+(provide 'ce-utils)
+
+;; Local Variables:
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+
+;;; ce-utils.el ends here

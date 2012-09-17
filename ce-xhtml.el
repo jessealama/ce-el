6df9dc757c4f06b5f5bd3784ffb6cd44e01b3c2b
Jesse Alama <jesse.alama@gmail.com>
2012-09-17 13:08:42 +0200

    Extract the character blocks from an XHTML document.

--- ce-xhtml.el
+++ ce-xhtml.el
@@ -1,5 +1,29 @@
 
+(require 'cl)
 (require 'nxml-mode)
+
+(defun ce-xhtml-character-blocks-of (thing)
+  (cond ((null thing) nil)
+	((symbolp thing) nil)
+	((stringp thing) (list thing))
+	((consp thing)
+	 (condition-case nil
+	     (destructuring-bind (element attributes . children)
+		 thing
+	       (reduce 'append (mapcar 'ce-xhtml-character-blocks-of children)))
+	   (error
+	    (error "Don't know how to extract the character blocks of '%s'" thing))))
+	(t
+	 (error "Don't know how to extract the character blocks of '%s'" thing))))
+
+(defun ce-xhtml-character-data-blocks ()
+  (let ((file (buffer-file-name)))
+    (unless file
+      (error "No file is associated with the current buffer."))
+    (let ((tree (condition-case nxml-parse-error
+		    (nxml-parse-file file)
+		  (error "Unable to parse %s.%cThe error was:%c%c%s" file ?\n ?\n ?\n (error-message-string nxml-parse-error)))))
+      (ce-xhtml-character-blocks-of tree))))
 
 (provide 'ce-xhtml)
 

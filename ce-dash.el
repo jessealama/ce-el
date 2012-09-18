6503f26fa0bfcf89704dbe90cb150fd8798be672
Jesse Alama <jesse.alama@gmail.com>
2012-09-18 17:57:21 +0200

    Use the XML-driven dash detector.

--- ce-dash.el
+++ ce-dash.el
@@ -1,45 +1,47 @@
 
 (require 'cl)
-(require 'nxml-mode) ; for navigating among XML tokens
+(require 'nxml-mode)
+(require 'ce-xhtml)
+(require 'ce-entities)
 
 (defun ce-dash-is-dash-entity (entity-str)
   (or (string= entity-str "&ndash;")
       (string= entity-str "&mdash;")))
 
-(defun ce-dash-next-dash ()
-  (let ((begin nil)
-	(end nil)
-	(dash-begin nil)
-	(dash-end nil)
-	(dash-found nil))
-    (let* ((begin (point-min))
-	   (previous-xml-token nil)
-	   (current-xml-token (xmltok-forward))
-	   (end (point)))
-      (while (and current-xml-token
-		  (not dash-found))
-	(let ((data (buffer-substring-no-properties begin end)))
-	  (cond ((eq current-xml-token 'entity-ref)
-		 (when (ce-dash-is-dash-entity data)
-		   (setf dash-begin begin
-			 dash-end end
-			 dash-found t)))
-		((and (eq previous-xml-token 'start-tag)
-		      (eq current-xml-token 'data))
-		 (let ((dash-position (position ?\- data)))
-		   (when dash-position
-		     (let ((local-end (string-match "[^-]" data dash-position)))
-		       ;; (error "Data is '%s'%cdash-position is %d%cand the the local end is '%s'" data ?\n dash-position ?\n local-end)
-		       (setf dash-begin (+ begin dash-position))
-		       (setf dash-end (1- (+ begin local-end)))
-		       (setf dash-found t)))))))
-	(setf begin end
-	      previous-xml-token current-xml-token
-	      current-xml-token (xmltok-forward)
-	      end (point))))
-    (when dash-found
-      (goto-char dash-begin)
-      (list dash-begin dash-end))))
+(defun ce-dash-inspect-string (string)
+  string)
+
+(defun ce-dash-inspect-nxml-thing (thing)
+  (cond ((stringp thing) (ce-dash-inspect-string thing))
+	((null thing) nil)
+	((consp thing)
+	 (let ((element nil)
+	       (attributes nil)
+	       (children nil))
+	   (condition-case nil
+	       (destructuring-bind (local-element local-attributes . local-children)
+		   thing
+		 (setf element local-element
+		       attributes local-attributes
+		       children local-children))
+	     (error
+	      (error "Unable to make sense of the node '%s'" thing)))
+	   (append (list element attributes)
+		   (mapcar 'ce-dash-inspect-nxml-thing children))))
+	(t
+	 (error "Don't know how to make sense of the nxml object '%s'" thing))))
+
+(defun ce-dash-inspect-dashes ()
+  (interactive)
+  (ce-entities-resolve-named-entities-decimally)
+  (save-buffer)
+  (let ((tree (condition-case nxml-parse-error
+		  (nxml-parse-file (buffer-file-name))
+		(error
+		 (error "Unable to parse the current buffer as XML:%c%c%s" ?\n ?\n (error-message-string nxml-parse-error))))))
+    (let ((new-tree (ce-dash-inspect-nxml-thing tree)))
+      (erase-buffer)
+      (insert (ce-xhtml-render-nxml-thing new-tree)))))
 
 (provide 'ce-dash)
 

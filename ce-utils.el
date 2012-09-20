556b3019f70b79c2b003370f3a88def4db05f4d5
Jesse Alama <jesse.alama@gmail.com>
2012-09-20 11:47:39 +0200

    Cursored strings.

--- ce-utils.el
+++ ce-utils.el
@@ -15,6 +15,45 @@
 		(string= thing (aref entity-array 2))))
 	list-of-arrays))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Strings with cursor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defclass cursored-string ()
+  ((string :initarg :string
+	   :initform (error "To create a cursored-string, a string must be provided.")
+	   :type string
+	   :documentation "The underlying string (sequence of characters).")
+   (cursor :initarg :initial-position
+	   :initform 0
+	   :type integer
+	   :documentation "The current position of the cursored string.")))
+
+(defmethod cs-length ((cs cursored-string))
+  (length (oref cs string)))
+
+(defconst +cs-render-separator+ "|"
+  "The string used to separate the characters of a rendered cursorsed-string.")
+
+(defmethod cs-discretize ((cs cursored-string))
+  (let ((string (copy-sequence (oref cs string)))
+	(cursor-pos (oref cs cursor)))
+    (with-output-to-string
+      (loop
+       initially (princ (format "%s" +cs-render-separator+))
+       for char across string
+       do
+       (princ (format " %c %s") char +cs-render-separator+)))))
+
+(defmethod range-of-cursor-in-digitized-string ((cs cursored-string))
+  (let ((cursor (ce-dash-position-in-digitized-string string position)))
+    (cons (- position-in-digitized 2)
+	  (+ position-in-digitized 1))))
+
+(defmethod cs-render ((cs cursored-string))
+  (let ((cursor-pos (oref cs cursor)))
+    (let ((discretized (cs-discretize cs))))))
+
 (provide 'ce-utils)
 
 ;; Local Variables:
